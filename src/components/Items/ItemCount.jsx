afdf40fe1c9bc99fbc2a0c67ccffdfe197eb6f59
alearbat <alearbat@gmail.com>
2021-04-24 10:28:17 -0300

    improving btn style

--- src/components/Items/ItemCount.jsx
+++ src/components/Items/ItemCount.jsx
@@ -5,18 +5,20 @@ const ItemCount = ({stock}) => {
 const [stockTotal, setStockTotal] = useState(stock)
 const [unitsToBuy, setUnitsToBuy] = useState(1)
 
+let className = ""
+
 const sum = () => {
   if(stockTotal > 0) {
     setUnitsToBuy(unitsToBuy + 1)
     setStockTotal(stockTotal - 1)
-  }
+  } 
 }
 
 const subtract = () => {
   if(unitsToBuy > 1) {
     setUnitsToBuy(unitsToBuy - 1)
     setStockTotal(stockTotal + 1)
-  }
+  } 
 }
 
 const onAdd = () => {
@@ -26,12 +28,12 @@ const onAdd = () => {
 
   return (
     <>
-      <div className="units">
-        <button className='btn btn-info' onClick={subtract} disabled={unitsToBuy === 1}>-</button>
+      <div className='units'>
+        <button className='btn' onClick={subtract} disabled={unitsToBuy === 1}>-</button>
         <p>{unitsToBuy}</p>
         <button className='btn' onClick={sum} disabled={stockTotal === 0}>+</button>
       </div>
-      <button className='btn btn-success' onClick={onAdd}>Agregar al carrito</button>
+      <button className='btn' onClick={onAdd}>Agregar al carrito</button>
     </>
   )
 }
