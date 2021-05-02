a3f2831dec1dfb11b043ea4068cd30e3301da1b6
alearbat <alearbat@gmail.com>
2021-05-02 12:12:19 -0300

    adding styles to Services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -6,11 +6,11 @@ const Services = () => {
   return (
     <>
       <section className="services row service-bg">
-        <h1 className="d-flex justify-content-center col-lg-12 col-md-12 col-xs-12 col-12">NUESTROS SERVICIOS</h1>
-        <p className="d-flex justify-content-center col-lg-12 col-md-12 col-xs-12 col-12">Brindamos asesoramiento personalizado en la compra grande, mediana y pequeña.</p>
-        <p className="d-flex justify-content-center col-lg-12 col-md-12 col-xs-12 col-12">Si sos paisajista​ o diseñador/a vas a encontrar la mejor variedad de plantas para todos tus proyectos.</p>
-        <p className="d-flex justify-content-center col-lg-12 col-md-12 col-xs-12 col-12">Si querés crear espacios únicos en tu hogar o buscas un regalo bien natural, vas a encontrar la mejor variedad de plantas y los elementos necesarios para cuidarlas, además de asesoramiento con nuestros especialistas.</p> 
-        <p className="d-flex justify-content-center col-lg-12 col-md-12 col-xs-12 col-12">Consultá por nuestros servicios de Jardines y Empresas</p>
+        <h1 className="title d-flex justify-content-center col-lg-12 col-md-12 col-xs-12 col-12">NUESTROS SERVICIOS</h1>
+        <p className="text d-flex justify-content-center col-lg-12 col-md-12 col-xs-12 col-12">Brindamos asesoramiento personalizado en la compra grande, mediana y pequeña.
+        <br/><br/>Si sos paisajista​ o diseñador/a vas a encontrar la mejor variedad de plantas para todos tus proyectos.
+        <br/><br/>Si querés crear espacios únicos en tu hogar o buscas un regalo bien natural, vas a encontrar la mejor variedad de plantas y los elementos necesarios para cuidarlas, además de asesoramiento con nuestros especialistas.
+        <br/><br/>Consultá por nuestros servicios de Jardines y Empresas</p>
       </section>
     </>
   )
